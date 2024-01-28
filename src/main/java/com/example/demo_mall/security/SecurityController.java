6e8a302ed768e58baedb21f10ca33c9e97b34651
shoney <ddmczp@naver.com>
2024-01-28 15:44:41 +0900

    back end init

---
+++ src/main/java/com/example/demo_mall/security/SecurityController.java
@@ -0,0 +1,26 @@
+package com.example.demo_mall.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+    @GetMapping("/admin/pay")
+    public String adminPay() {
+        return "admin Pay";
+    }
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
