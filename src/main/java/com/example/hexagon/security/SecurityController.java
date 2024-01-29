2b2575b45534d19b42f38f7983491f39f8342770
shoney <ddmczp@naver.com>
2024-01-29 20:26:58 +0900

    chore: 스웨거 적용

--- src/main/java/com/example/hexagon/security/SecurityController.java
+++ src/main/java/com/example/hexagon/security/SecurityController.java
@@ -1,8 +1,9 @@
 package com.example.hexagon.security;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Hidden
 @RestController
 public class SecurityController {
 
